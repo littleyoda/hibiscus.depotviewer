c76914c7713a30a61d2b14ca174a0499c33ceb27
Sven Bursch-Osewold <git@bursch.com>
2014-12-08 09:19:34 +0100

    Anpassungen wegen neuer Webseite und URL

--- src/de/open4me/depot/abruf/impl/CortalConsorsMitHBCI.java
+++ src/de/open4me/depot/abruf/impl/CortalConsorsMitHBCI.java
@@ -1,5 +1,8 @@
 package de.open4me.depot.abruf.impl;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.rmi.RemoteException;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -9,21 +12,23 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
+
+import com.gargoylesoftware.htmlunit.HttpMethod;
 import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.ThreadedRefreshHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.DomNodeList;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
-import com.gargoylesoftware.htmlunit.html.HtmlTableCell;
-import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
 
 import de.open4me.depot.DepotViewerPlugin;
+import de.open4me.depot.abruf.utils.HtmlUtils;
+import de.open4me.depot.abruf.utils.PropHelper;
 import de.open4me.depot.abruf.utils.Utils;
 import de.willuhn.jameica.hbci.rmi.Konto;
 import de.willuhn.jameica.system.Application;
@@ -52,6 +57,12 @@ public class CortalConsorsMitHBCI extends BasisHBCIDepotAbruf {
 
 	@SuppressWarnings("unchecked")
 	public void runUmsaetze(Konto konto) throws ApplicationException {
+		String kontonummer = null;
+		try {
+			kontonummer = konto.getKontonummer();
+		} catch (RemoteException e2) {
+			throw new ApplicationException("Kontonummer nicht gefunden", e2);
+		}
 		ArrayList<String> seiten = new ArrayList<String>(); 
 		try {
 			String username = konto.getMeta(PROP_KUNDENNUMMER, null);
@@ -72,82 +83,76 @@ public class CortalConsorsMitHBCI extends BasisHBCIDepotAbruf {
 			final WebClient webClient = new WebClient();
 			webClient.setCssErrorHandler(new SilentCssErrorHandler());
 			webClient.setRefreshHandler(new ThreadedRefreshHandler());
-			webClient.getOptions().setJavaScriptEnabled(false); 
-			HtmlPage page = webClient.getPage("https://mobile.cortalconsors.de/euroWebDe/-?$part=sslm.login&s_requestedURL=https://mobile.cortalconsors.de/euroWebDe/-?$part=sslm.MobileDefault.depot&$event=orderInfoEntry");
-			seiten.add(page.asXml());
-			List<HtmlForm> forms = (List<HtmlForm>) page.getByXPath("//form[@id='login']");
-			if (forms.size() != 1) {
-				Utils.report((List<? extends HtmlElement>) page.getByXPath("//form"));
-				throw new ApplicationException("Konnte das Login-Formular nicht finden.");
-			}
-			HtmlForm form = forms.get(0);
-			form.getInputByName("userId").setValueAttribute(username);
-			form.getInputByName("nip").setValueAttribute(password);
-			HtmlInput input = form.getInputByName("$$event_contentAreaCustomerLoginMobile");
-			page = input.click();
-			seiten.add(page.asXml());
+			webClient.getOptions().setJavaScriptEnabled(false);
 
-			// Nach dem Link für das passende Depot suchen. Wichtig, wenn man mehr als ein depot hat
-			HtmlAnchor link = null;
-			for (HtmlAnchor x : page.getAnchors()) {
-				if (x.asText().equals("" + konto.getKontonummer())) {
-					link = x;
-				}
-			}
-			if (link == null) {
-				throw new ApplicationException("Link zum Konto " + konto.getKontonummer() + " nicht gefunden!");
-			}
-			page = link.click();
+			// Viel Ajax auf der Webseite, mit dem HTMLUnit nicht zu recht kommt.
+			// Also brauchen wir uns den Login-Request selber zusammen
+			HtmlPage page = webClient.getPage("https://www.consorsbank.de/ev/System/Login?showEVLoginForm=true");
 			seiten.add(page.asXml());
+			WebRequest requestSettings = new WebRequest(new URL("https://www.consorsbank.de/euroWebDe/-?$part=login.json&$event=login"), HttpMethod.POST);
+			requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
+			requestSettings.getRequestParameters().add(new NameValuePair("userId", username));
+			requestSettings.getRequestParameters().add(new NameValuePair("nip", password));
+			requestSettings.getRequestParameters().add(new NameValuePair("sender", ""));
+			requestSettings.getRequestParameters().add(new NameValuePair("referer", ""));
+			requestSettings.setAdditionalHeader("Referer", "https://www.consorsbank.de/ev/System/Login?showEVLoginForm=true");
+			requestSettings.setAdditionalHeader("X-Requested-With", "XMLHttpRequest");
+			requestSettings.setAdditionalHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			com.gargoylesoftware.htmlunit.UnexpectedPage p = webClient.getPage(requestSettings);
+			StringWriter writer = new StringWriter();
+			IOUtils.copy(p.getInputStream(), writer, Charset.forName("UTF-8"));
+			// TODO Json Antwort prüfen
+			seiten.add(writer.toString());
 
-			try {
-				page = page.getAnchorByText("Orderinfo").click();
-			} catch (com.gargoylesoftware.htmlunit.ElementNotFoundException e) {
-				Utils.report(page.getAnchors());
-				throw new ApplicationException("Orderinfo nicht gefunden!");
-			}
 
+
+			// Und die Orderübersicht hart reincodieren.
+			page = webClient.getPage("https://www.consorsbank.de/euroWebDe/-?$part=MonalisaDE.Desks.InvestmentAccounts.Desks.OrderInfo.content.orderInfo.main.accountTabs&$event=selectAccountViaRequestParam&accountno=" + kontonummer);
+			seiten.add(page.asXml());
 			HashMap<String, String> infos = new HashMap<String, String>();
 			DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
 			boolean missingOrderDate = false;
 			for (HtmlAnchor x : page.getAnchors()) {
-				if (x.getAttribute("href").contains("orderInfoPageNumber")) {
-					infos.clear();
-					page =  x.click();
-					seiten.add(page.asXml());
-					DomNodeList<DomElement> list = page.getElementsByTagName("table");
-					if (list.size() != 3) {
-						//throw new ApplicationException("Anzahl der Tabelen in Orderinfo stimmt nicht. Ist: " + list.size());
-					}
-					tohash(infos, (HtmlTable) list.get(2), false);
-					tohash(infos, (HtmlTable) list.get(3), true);
-					String[] kurs = ((String) infos.get("kurs")).replaceAll("  *", " ").split(" ");
-					if (!infos.containsKey("order vom")) {
-						missingOrderDate = true;
-						continue;
-					}
-					Date d;
-					try {
-						d = df.parse(infos.get("order vom").substring(0,10));
-
-						//						d = df.parse(infos.get("zeitpunkt der abrechnung").substring(0,10));
-					} catch (ParseException e) {
-						throw new ApplicationException("Unbekanntes Datumsformat beim Abrechnungszeitpunkt: " + infos.get("zeitpunkt der abrechnung"));
-					}
-
-
-					Utils.addUmsatz(konto.getID(), Utils.getORcreateWKN(infos.get("wkn"), "", infos.get("wertpapiername")), infos.get("orderart"), 
-							infos.toString(),
-							Utils.getDoubleFromZahl(infos.get("stück")),
-							Utils.getDoubleFromZahl(kurs[0]), kurs[1],
-
-							((infos.get("orderart").toUpperCase().equals("KAUF")) ? -1 : 1) *
-							Math.rint(Utils.getDoubleFromZahl(kurs[0]) * Utils.getDoubleFromZahl(infos.get("stück")) * 100) / 100,
-							kurs[1],
-							d,
-							infos.get("ordernummer"), ""
-							);
+				if (!x.getAttribute("href").contains("event=details&orderInfoPageNumber=1&orderno=")) {
+					continue;
 				}
+				infos.clear();
+				page =  x.click();
+				seiten.add(page.asXml());
+				DomNodeList<DomElement> list = page.getElementsByTagName("table");
+//				if (list.size() != 3) {
+//					//throw new ApplicationException("Anzahl der Tabelen in Orderinfo stimmt nicht. Ist: " + list.size());
+//				}
+				HtmlUtils.tabUntereinander2hash(infos, (HtmlTable) list.get(1), 0, 1); // Spalte 1 und 2
+				HtmlUtils.tabUntereinander2hash(infos, (HtmlTable) list.get(1), 3, 4); // Spalte 4 und 5; Spalte 3 ist leer
+				HtmlUtils.tabNebeneinander2hash(infos, (HtmlTable) list.get(2));
+				System.out.println(infos.entrySet());
+				String[] kurs = ((String) infos.get("kurs")).replaceAll("  *", " ").split(" ");
+				if (!infos.containsKey("zeitpunkt der abrechnung")) {
+					missingOrderDate = true;
+					continue;
+				}
+				Date d;
+				try {
+					d = df.parse(infos.get("zeitpunkt der abrechnung").substring(0,10));
+
+					//						d = df.parse(infos.get("zeitpunkt der abrechnung").substring(0,10));
+				} catch (ParseException e) {
+					throw new ApplicationException("Unbekanntes Datumsformat beim Abrechnungszeitpunkt: " + infos.get("zeitpunkt der abrechnung"));
+				}
+
+
+				Utils.addUmsatz(konto.getID(), Utils.getORcreateWKN(infos.get("wkn"), infos.get("isin"), infos.get("wertpapiername")), infos.get("orderart"), 
+						infos.toString(),
+						Utils.getDoubleFromZahl(infos.get("stück/nominale")),
+						Utils.getDoubleFromZahl(kurs[0]), kurs[1],
+
+						((infos.get("orderart").toUpperCase().equals("KAUF")) ? -1 : 1) *
+						Math.rint(Utils.getDoubleFromZahl(kurs[0]) * Utils.getDoubleFromZahl(infos.get("stück/nominale")) * 100) / 100,
+						kurs[1],
+						d,
+						infos.get("ordernummer"), ""
+						);
 			}
 
 			if (missingOrderDate) {
@@ -168,54 +173,13 @@ public class CortalConsorsMitHBCI extends BasisHBCIDepotAbruf {
 	}
 
 
-	private void tohash(HashMap<String, String> infos, HtmlTable tab,
-			boolean b) {
-		if (!b) {
-			for (HtmlTableRow row :tab.getRows()) {
-				List<HtmlTableCell> cells = row.getCells();
-				if (cells.size() == 1 && row.asText().toLowerCase().contains("zurück")) {
-					continue;
-				}
-				if (cells.size() != 2) {
-					Logger.info("Warnung. Ungültige Anzahl an Zellen: " + cells.size() + " " + row.asText());
-					continue;
-				}
-				infos.put(cells.get(0).asText().toLowerCase(), cells.get(1).asText());
-			}
-			return;
-		}
-		List<HtmlTableRow> rows = tab.getRows();
-		if (rows.size() < 2) {
-			System.out.println("Warnung. Ungültige Anzahl an Zeilen: " + rows.toString());
-			return;
-		}
-		List<HtmlTableCell> r1 = rows.get(0).getCells();
-		for (int zeile = 1; zeile < rows.size(); zeile++) {
-			List<HtmlTableCell> r2 = rows.get(zeile).getCells();
-			if (r1.size() != r2.size()) {
-				Logger.info("Warnung. Ungültige Anzahl an Zellen: " + r1.toString());
-				continue;
-			}
-			int missing=0;
-			for (int i = 0; i < r1.size(); i++) {
-				String header = r1.get(i).asText().toLowerCase();
-				if ("".equals(header)) {
-					header = "Missing" + missing;
-					missing++;
-				}
-				infos.put(header, r2.get(i).asText());
-			}
-		}
-
-	}
-
 
 	@Override
 	public List<String> getPROP() {
 		List<String> result = super.getPROP();
 		result.add(0, PROP_PASSWORD);
 		result.add(0, PROP_KUNDENNUMMER);
-		//result.remove(PropHelper.UMSAETZEERGAENZENINCLFORMAT);
+		result.add(PropHelper.NURBESTANDINKLFORMAT);
 		return result;
 	}
 
