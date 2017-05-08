60ce57f968d8ca9967dfba23564157c4a31f3072
Sven Bursch-Osewold <sb_git@bursch.com>
2017-05-08 17:49:13 +0200

    31.12 als Auswahlzeitpunkt bei dem Bestand hinzugefügt

--- src/de/open4me/depot/gui/control/BestandsControl.java
+++ src/de/open4me/depot/gui/control/BestandsControl.java
@@ -2,7 +2,9 @@ package de.open4me.depot.gui.control;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import de.open4me.depot.sql.GenericObjectSQL;
@@ -18,9 +20,19 @@ public class BestandsControl extends AbstractControl {
 
 	public ArrayList<Date> getDates() throws RemoteException {
 		ArrayList<Date> datelist = new ArrayList<Date>(); 
+		Date last = null; 
 		List<GenericObjectSQL> list = SQLUtils.getResultSet("select distinct buchungsdatum from depotviewer_umsaetze order by buchungsdatum", "", "");
 		for (GenericObjectSQL x : list) {
-			datelist.add((Date) x.getAttribute("buchungsdatum"));
+			Date current = (Date) x.getAttribute("buchungsdatum");
+			System.out.println(current.getClass());
+			if (last != null && (last.getYear() != current.getYear())) {
+				Date jahresEnde = (Date) last.clone();
+				jahresEnde.setMonth(11);
+				jahresEnde.setDate(31);
+				datelist.add(jahresEnde);
+			}
+			datelist.add(current);
+			last = current;
 		}
 		return datelist;
 	}
