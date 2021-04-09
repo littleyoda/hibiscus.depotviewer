2fc6f256d1dfb2d4cc1ec1fb2fbf37ecd37d0a53
Fabian Aiteanu <63024793+faiteanu@users.noreply.github.com>
2021-04-09 23:19:15 +0200

    Verknüfung zwischen DepotViewer-Umsatz und Hibiscus-Umsatz erfolgt nun über Metadaten (depotviewer_id), statt der EndToEndID.

--- src/de/open4me/depot/tools/UmsatzHelper.java
+++ src/de/open4me/depot/tools/UmsatzHelper.java
@@ -30,13 +30,12 @@ public class UmsatzHelper {
 	 * @throws ApplicationException
 	 */
 	public static void storeUmsatzInHibiscus(Umsatz u) throws RemoteException, ApplicationException {		
-		de.willuhn.jameica.hbci.rmi.Umsatz hu = null;
-		// abfragen, ob Umsatz bereits existiert anhand der EndToEndId
-		DBIterator<de.willuhn.jameica.hbci.rmi.Umsatz> liste = Settings.getDBService().createList(de.willuhn.jameica.hbci.rmi.Umsatz.class);
-		liste.addFilter("endtoendid=?", "DVID" + u.getID());
-		if(liste.hasNext())
-			hu = liste.next();
-		else {
+		VarDecimalFormat df = new VarDecimalFormat(2);
+		VarDecimalFormat kf = new VarDecimalFormat(5);
+		
+		// abfragen, ob Umsatz bereits existiert anhand der meta-depotviewer_id
+		de.willuhn.jameica.hbci.rmi.Umsatz hu = getHibiscusUmsatzByDepotViewerId(u.getID());
+		if(hu == null) {
 			hu = (de.willuhn.jameica.hbci.rmi.Umsatz) Settings.getDBService().createObject(de.willuhn.jameica.hbci.rmi.Umsatz.class,null);
 			hu.setArt("WERTPAPIERE");
 			hu.setFlags(de.willuhn.jameica.hbci.rmi.Umsatz.FLAG_NONE);
@@ -52,17 +51,16 @@ public class UmsatzHelper {
 			hu.setCustomerRef(null);
 			hu.setMandateId(null);
 			hu.setUmsatzTyp(null);
-			
-			hu.setEndToEndId("DVID" + u.getID()); // DVID = DepotViewer ID
+
 			hu.setKommentar("Automatisch erzeugt von DepotViewer");
-			hu.setSaldo(0); 
+			hu.setSaldo(0);
 		}
 	    hu.setBetrag(-u.getKosten().doubleValue());
 	    hu.setDatum(u.getBuchungsdatum());
 	    hu.setValuta(u.getBuchungsdatum());
 	    
 	    Wertpapier w = Utils.getWertPapierByID(u.getWPid().toString());
-	    hu.setZweck(u.getAktion().toString() + " " + u.getAnzahl().toString() + " STK");
+	    hu.setZweck(u.getAktion().toString() + " " + kf.format(u.getAnzahl()) + " STK");
 	    hu.setZweck2("WKN: " + w.getWkn() + " ISIN: " + w.getIsin());
 	    
 	    BigDecimal gegenwert = u.getKosten();
@@ -74,12 +72,13 @@ public class UmsatzHelper {
 	    	ertragsart = "Erlös";
 	    }
 	    
+	    
 	    hu.setWeitereVerwendungszwecke(VerwendungszweckUtil.parse(w.getWertpapiername() + "\n"
-	    		+ "Kurs: " + u.getKurs().toString() + " " + u.getKursW() + "\n" 
-	    		+ "Betrag: " + u.getKosten().negate().toString() + " " + u.getKostenW() + "\n"
-	    		+ "Steuern: " + u.getSteuern().toString() + " " + u.getSteuernW() + "\n"
-	    		+ "Gebühren: " + u.getTransaktionsgebuehren() + " " + u.getTransaktionsgebuehrenW() + "\n"
-	    		+ ertragsart + ": " + gegenwert  + " " + u.getKostenW() + "\n"));	    
+	    		+ "Kurs: " + kf.format(u.getKurs()) + " " + u.getKursW() + "\n" 
+	    		+ "Betrag: " + df.format(u.getKosten().abs()) + " " + u.getKostenW() + "\n"
+	    		+ "Steuern: " + df.format(u.getSteuern()) + " " + u.getSteuernW() + "\n"
+	    		+ "Gebühren: " + df.format(u.getTransaktionsgebuehren()) + " " + u.getTransaktionsgebuehrenW() + "\n"
+	    		+ ertragsart + ": " + df.format(gegenwert)  + " " + u.getKostenW() + "\n"));	    
 	    
 	    // Saldo berechnen, falls möglich
 	    Bestandsabfragen abfragen = new Bestandsabfragen();
@@ -89,5 +88,25 @@ public class UmsatzHelper {
 	    
 	    
 	    hu.store();
+	    
+	    hu.setMeta("depotviewer_id", u.getID()); // Meta-Daten werden getrennt vom eigentlichen Umsatz (nicht mit store()) gespeichert.
+	}
+	
+	/**
+	 * Ermittelt den Hibiscus-Umsatz, der zu einem DepotViewer-Umsatz gehört.
+	 * Aktuell gehen wir davon aus, dass es eine 1:1 Beziehung ist.
+	 * @param depotViewerId
+	 * @return Der Umsatz aus Hibiscus oder null, falls es keinen passenden gibt.
+	 * @throws RemoteException
+	 */
+	public static de.willuhn.jameica.hbci.rmi.Umsatz getHibiscusUmsatzByDepotViewerId(String depotViewerId) throws RemoteException{
+		DBIterator<de.willuhn.jameica.hbci.rmi.Umsatz> liste = Settings.getDBService().createList(de.willuhn.jameica.hbci.rmi.Umsatz.class);	
+		while(liste.hasNext()) {
+			de.willuhn.jameica.hbci.rmi.Umsatz hu = liste.next();
+			String dvId = hu.getMeta("depotviewer_id", null);
+			if(depotViewerId.equals(dvId))
+				return hu;
+		}
+		return null;
 	}
 }
